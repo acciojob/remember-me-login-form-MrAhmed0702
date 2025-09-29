fb033633d8e3ec1061008191ff8a468cc223d0e6
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-09-30 01:05:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded}', function() {
+	const loginForm = document.getElementById('loginForm');
+    const submitButton = document.getElementById('submit');
+    const existingButton = document.getElementById('existing');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const rememberMeCheckbox = document.getElementById('checkbox');
+
+	const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+
+	if(storedPassword && storedPassword){
+		existingButton.style.display = 'block';
+	}
+
+	loginForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		
+		alert(`Logged in as ${usernameInput.value}`);
+		
+		if(rememberMeCheckbox.checked){
+			localStorage.setItem('username', usernameInput.value);
+			localStorage.setItem('password', passwordInput.value);
+		} else {
+			localStorage.removeItem('username');
+            localStorage.removeItem('password');
+		}
+	});
+
+	existingButton.addEvenListener('click', () => {
+		if(storedUsername){
+			alert(`Logged in as ${storedUsername}`);
+		}
+	})
+});
